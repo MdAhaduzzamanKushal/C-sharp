9db7325f66be1600c04868d71a1fa94f3ed776de
MdAhaduzzamanKushal <kushal.tay@gmail.com>
2023-06-07 19:09:38 +0600

    Array index[0]

---
+++ array(1).cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            int[] luckyNumbers = { 1, 2 };
+            Console.WriteLine(luckyNumbers);
+            Console.ReadLine();
+        
+        }
+    }
+}
